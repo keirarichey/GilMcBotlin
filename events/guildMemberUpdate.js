cf24fd6d486c1e5a13c543f8c0cc24210f6d7aff
ZedFish <miller2502@gmail.com>
2022-01-01 19:43:35 +1000

    formatting

--- events/guildMemberUpdate.js
+++ events/guildMemberUpdate.js
@@ -48,6 +48,7 @@ module.exports = {
         if (oldMember.partial) {
             return;
         }
+        
         if (newMember.roles.cache.some(role => removeEmojis(role.name) === entryRoleName) && !oldMember.roles.cache.some(role => removeEmojis(role.name) === entryRoleName)) {
             // If someone has been given the entry role
             console.log(oldMember.displayName);
