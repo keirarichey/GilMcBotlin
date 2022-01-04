4486a2a60a7730a236ffe88100591c1ee6852880
ZedFish <miller2502@gmail.com>
2022-01-04 13:43:31 +1000

    Refresh presence on a loop

--- events/ready.js
+++ events/ready.js
@@ -13,7 +13,11 @@ module.exports = {
         console.log(`--------------------------------------------------------`);
         console.log(`Logged in and successfully connected as ${client.user.username}.`);
         console.log(`Invite link: https://discord.com/api/oauth2/authorize?client_id=${client.user.id}&permissions=8&scope=applications.commands%20bot`);
-        client.user.setPresence({ activities: [{ type: 'PLAYING', name: `with ${new Date().getFullYear() + 1} AFL rules` }], status: 'online' });
+
+        // Set bot's activity on a 30-minute loop: this is to prevent it just disappearing
+        setInterval(() => {
+            client.user.setPresence({ activities: [{ type: 'PLAYING', name: `with ${new Date().getFullYear() + 1} AFL rules` }], status: 'online' });
+        }, 1800000);
 
         // Check database and initialise all tables if they don't exist
         const databaseTables = fs.readdirSync('./tables').filter(file => file.endsWith('.js'));
