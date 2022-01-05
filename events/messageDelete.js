0244c8a82af9d60f9667eac4f5f2211a01f651b3
ZedFish <miller2502@gmail.com>
2022-01-05 12:37:28 +1000

    add member name to deleted message

--- events/messageDelete.js
+++ events/messageDelete.js
@@ -28,13 +28,19 @@ module.exports = {
         }
 
         const messageGuildMember = await message.guild.members.cache.find(member => member.id === message.author.id);
-        
+        await messageGuildMember.fetch()
+            .catch(err => {
+                console.error(err);
+                return;
+            });
+
         const embed = new MessageEmbed()
             .setThumbnail(message.author.avatarURL())
+            .setTitle(`${messageGuildMember.displayName}`)
             .setColor(0x345b95) // standard blue
             .setDescription(`**Message by ${messageGuildMember.toString()} in ${message.channel.toString()}:**\n${message.content}`)
             .setTimestamp();
-        
+
         await message.guild.channels.cache.find(channel => channel.name === deletedMessageChannelName)?.send({ embeds: [embed] })
             .catch(err => console.error(err));
         
